ca17f9a97df9828ace2b6b5ca7cd0f334623ba44
Gar Conklin <gar_conklin@hotmail.com>
2026-06-25 23:00:26 -0300

    Archive one-off enumeration scripts to local unused folder.
    Remove dated run/list/gen scripts from git and ignore unused/ so copies stay local only.

--- scripts/list-may30-new-level-ids.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-/** Four new 6×6 levels from ingest of data/tilepz solves 30 may 2026a.txt */
-'use strict';
-const IDS = ['6x6-0B-ABY', '6x6-0B-ABZ', '6x6-0B-ACA', '6x6-0B-ACB'];
-for (const id of IDS) console.log(id);
